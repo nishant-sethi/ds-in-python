2454ab210b385500a27d03eb3d49c8e828bbcb37
Nishant sethi <nsethi610@gmail.com>
2022-08-12 12:29:33 +0100

    Create algo.py

---
+++ algo.py
@@ -0,0 +1,24 @@
+class Sorting:
+    def bubble_sort(self,list_of_elements):
+        pass
+    def insertion_sort(self,list_of_elements):
+        pass
+    def selection_sort(self,list_of_elements):
+        pass
+    def merge_sort(self,list_of_elements):
+        pass
+    def quick_sort(self,list_of_elements):
+        pass
+    def counting_sort(self,list_of_elements):
+        pass
+    def radix_sort(self,list_of_elements):
+        pass
+    def shell_sort(self,list_of_elements):
+        pass
+
+class Searching:
+    def linear_seach(self,list_of_elements):
+        pass
+    def binary_search(self,list_of_elements):
+        pass
+    
