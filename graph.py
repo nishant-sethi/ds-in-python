9b7c4f1a068a25e906179ea1ecebc331e75b5f49
Nishant sethi <nsethi610@gmail.com>
2022-08-12 12:28:29 +0100

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,27 @@
+class Graph:
+    def add_node_to_graph(self):
+        pass
+
+    def delete_node_from_graph(self):
+        pass
+
+    def bfs_traversal(self):
+        pass
+
+    def dfs_traversal(self):
+        pass
+
+    def minimum_spanning_tree(self):
+        pass
+
+    def dijsktra(self):
+        pass
+
+    def bellman_ford(self):
+        pass
+
+class UndirectedGraph(Graph):
+    pass
+
+class DirectedGraph(Graph):
+    pass
